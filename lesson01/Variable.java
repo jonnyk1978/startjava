10e59df90a1c5ae9455ba112b3d5342668aaa24d
Евгений С. Красюк <jonny@afina-pallada.local>
2020-01-13 15:40:44 +0700

    first commit

---
+++ lesson01/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+	
+	public static void main(String[] args) {
+
+		byte    b = 4;
+		short   s = 3200;
+		int     i = 240;
+		long    l = -39187;
+		float   f = 17.844f;
+		double  d = 88.99999912d;
+		char    c = 'Q';
+		boolean bl = false;
+
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bl);
+	}
+}
